95d598f2bb7cb8397aa6eb8790690acf9e7870d7
Delan Azabani <delan@azabani.com>
2013-08-11 14:05:15 +0800

    completed prac 3/4: integrate IShed, ShedIron and ShedNickel

---
+++ IShed.java
@@ -0,0 +1,27 @@
+// The following test harness has been trivially modified to suit the class and
+// method design of the production code. Changes marked with "MODIFICATION".
+
+import java.util.*;	// for Iterator and Iterable
+
+/**
+ * IShed is the interface for all Sheds in the STMining application
+ *
+ * @author	Patrick Peursum
+ * @version	5.0
+ */
+
+
+public interface IShed extends Iterable<OrePile> {
+   public String getName();
+   public void setName(String newName);
+   // MODIFICATION: change type from int to OreType.
+   public OreType getOreType();
+   
+   public void addPile(OrePile newPile);
+   public double satisfyOrder(ShipmentOrder order);
+   
+   public double calcTotalOreWeight();
+   public double calcTotalMetalWeight();
+
+   public Iterator<OrePile> iterator();
+}
